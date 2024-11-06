c5f7b52e3245b53772e9838047fc48278c138d6b
Mother1teresa <129117416+Mother1teresa@users.noreply.github.com>
2024-11-06 18:49:48 +0300

    update js main

--- js/portfolio-block.js
+++ js/portfolio-block.js
@@ -4,20 +4,16 @@ document.addEventListener('DOMContentLoaded', () => {
     let currentIndex = 0;
 
     function updateSlider() {
-        const isMobile = window.innerWidth <= 420; // Проверяем, находимся ли мы на мобильном устройстве
-        const imageWidth = isMobile ? 90.933 + 0.447 : 39.323 + 2; // Ширина изображения в vw
-        const totalWidth = imageWidth; // Полная ширина одного изображения без отступов
-
-        // Вычисляем смещение
+        const isMobile = window.innerWidth <= 420; 
+        const imageWidth = isMobile ? 90.933 + 0.447 : 39.323 + 2; 
+        const totalWidth = imageWidth;
         let offset = -currentIndex * totalWidth;
-
         portfolioContainer.style.transform = `translateX(${offset}vw)`;
     }
 
     updateSlider();
 
     document.getElementById('nextBtn').addEventListener('click', () => {
-        // Проверяем, не достигли ли мы последнего изображения
         if (currentIndex < images.length - 1) {
             currentIndex++;
             updateSlider();
@@ -25,7 +21,6 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     document.getElementById('prevBtn').addEventListener('click', () => {
-        // Проверяем, не находимся ли мы на первом изображении
         if (currentIndex > 0) {
             currentIndex--;
             updateSlider();
